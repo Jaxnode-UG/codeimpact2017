07a00d938c5998d0ad2ecc86569182c616f0dd14
David Fekke <david@fekke.com>
2017-08-16 15:21:27 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample code from CodeImpact 2017
+
+Here are some samples from the Node.js for .NET Developers talk.
---
+++ index.js
@@ -0,0 +1,6 @@
+// Why Node is Cool!
+// Ryan Dahl example from JSConf 2009
+
+setTimeout(() => console.log('Hello CodeImpact, I am not blocking'), 1000);
+
+console.log('Hello Codeimpact');
